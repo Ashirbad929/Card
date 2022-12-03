70831cd0c6da61f592ead2925058bb9545e1dee7
Ashirbad <ashirbad929@gmail.com>
2022-12-03 22:26:17 +0530

    deploy2

--- src/Socialicons.jsx
+++ src/Socialicons.jsx
@@ -3,10 +3,10 @@ import {FaFacebook} from "react-icons/fa";
 const Socialicons = () => {
     return ( 
         <div>
-            <a className="icons" href="#">{FaFacebook}</a>
-            <a className="icons" href="#">{FaFacebook}</a>
-            <a className="icons" href="#">{FaFacebook}</a>
-           
+            <a className="icons" href="#"><FaFacebook/></a>
+            <a className="icons" href="#"><FaFacebook/></a>
+            <a className="icons" href="#"><FaFacebook/></a>
+          
 
         </div>
     );
