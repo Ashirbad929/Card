1e7d02cfd8ed892aa8ace3598baf96f5279246aa
Ashirbad <ashirbad929@gmail.com>
2022-12-03 22:44:01 +0530

    deploy2

--- src/Des.jsx
+++ src/Des.jsx
@@ -1,6 +1,8 @@
+import person from "./image/co.png"
 const Des = () => {
     return ( 
         <div>
+            <img className="img" src={person} alt="asdasd" />
             <h1>Ashirbad Behera</h1>
             <h4>Ashirbad929.website</h4>
         </div>
