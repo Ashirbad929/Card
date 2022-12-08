f13a51228bd1c8ecece6b8897c19cff529430fb9
Ashirbad <ashirbad929@gmail.com>
2022-12-09 00:29:28 +0530

    almost_ready

--- src/Des.jsx
+++ src/Des.jsx
@@ -1,11 +1,18 @@
 import person from "./image/co.png"
+import { FaLinkedinIn } from "react-icons/fa";
 const Des = () => {
     return ( 
         <div className="des">
             <img className="img" src={person} alt="asdasd" />
-            <h1>Ashirbad Behera</h1>
+            <h1 className="name">Ashirbad Behera</h1>
             <h2>Programmer/Developer</h2>
             <h4 className="website">Ashirbad929.website</h4>
+            <div className="llinks">
+           <a href="#"><FaLinkedinIn/></a>
+           <a href="#"><FaLinkedinIn/></a>
+            </div>
+            
+          
         </div>
      );
 }
