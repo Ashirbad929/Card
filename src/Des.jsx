7ef56f381328dcad49583c992c244dbf9ad3b11e
Ashirbad <ashirbad929@gmail.com>
2022-12-09 10:54:20 +0530

    added_business_links

--- src/Des.jsx
+++ src/Des.jsx
@@ -1,18 +1,25 @@
 import person from "./image/co.png"
 import { FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineMail} from "react-icons/ai";
 const Des = () => {
     return ( 
         <div className="des">
             <img className="img" src={person} alt="asdasd" />
             <h1 className="name">Ashirbad Behera</h1>
-            <h2>Programmer/Developer</h2>
-            <h4 className="website">Ashirbad929.website</h4>
+            <h3>Programmer/Developer</h3>
+            <h5 className="website">Ashirbad929.website</h5>
             <div className="llinks">
-           <a href="#"><FaLinkedinIn/></a>
-           <a href="#"><FaLinkedinIn/></a>
-            </div>
-            
           
+            <button className="butt">
+            <a 
+           className="ll" href="#"><AiOutlineMail/><small>Email</small></a>
+            </button>
+            
+          <button className="butt">
+          <a className="ll" href="#"><FaLinkedinIn/><small>LinkedIn</small></a>
+          </button>
+          
+        </div>
         </div>
      );
 }
